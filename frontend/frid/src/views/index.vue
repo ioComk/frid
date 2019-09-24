c584c63c4d77911a4378b26fa0fa4e4bf0b2fb5c
ioComk <rw110407@gmail.com>
2019-09-24 16:05:27 +0900

    Add: index.vue

---
+++ frontend/frid/src/views/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="index">
+    
+    <img class="logo" src="../assets/logo.png">
+    <el-menu
+      class="frid-nav"
+      mode="horizontal"
+    >
+      <el-menu-item index="1">About</el-menu-item>
+      <el-menu-item index="2">Features</el-menu-item>
+      <el-menu-item index="3">
+          <a href="#/signin">Get Started</a>
+          </el-menu-item>
+      <el-menu-item index="4">
+        <a href="https://github.com/frid-smartware/frid" target="_blank">GitHub</a>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+
+<style scoped>
+
+.logo{
+    display: inline-flex;
+    max-width: 120px;
+}
+
+/* .frid-nav{
+} */
+
+</style>
